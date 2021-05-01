0f92119359f15dce25fd667e60dec2612f2ca6a5
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 10:42:00 +0530

    Update Activity2.h

--- inc/Activity2.h
+++ inc/Activity2.h
@@ -1,7 +1,22 @@
-#ifndef INC2_H_INCLUDED
-#define INC2_H_INCLUDED
+#ifndef Activity2_H_INCLUDED
+#define Activity2_H_INCLUDED
+#define ADC_ENABLE (ADEN)
 
+/**
+ * @brief Checking for ADC interrupt occurrence
+ * 
+ */
+#define ADC_INTERRUPT_NOT_OCCURRED !(ADCSRA & (1 << ADIF))
+
+/**
+ * @brief Setting ADC interrupt flag
+ * 
+ */
+#define SET_ADC_INTERRUPT_FLAG ADCSRA |= (1 << ADIF);
+
+void ADC_init();
 uint16_t ReadADC(uint8_t ch);
-void InADC();
+#endif // Activity2_H_INCLUDED
 
-#endif // INC2_H_INCLUDED
+
+
