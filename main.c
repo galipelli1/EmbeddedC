6d6ecfe8f29fffafed09a964514d616ca5f471c8
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 10:50:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	/* Initialize Peripherals */
+	
 	init_port();
         Init_ADC_init();
 	Init_PWM();
@@ -17,7 +17,7 @@ int main(void)
 while (1)
     {
         uint16_t var = 0;
-        char temperature_op;
+        char temperature;
 
         //Activity-1
         sensor(); 
@@ -26,8 +26,8 @@ while (1)
         if (sensor() == 1)
         {
             var = Read_ADC(val_ADC);           //READS PWM OUTPUT
-            temperature_op = PWM_Reading(var); //CALCULATES TEMPERATURE VALUE
-            displayOUTPUT(temperature_op);     //DISPLAYS TEMPERATURE OUTPUT
+            temperature = InitPWM(var); //CALCULATES TEMPERATURE VALUE
+            displayOUTPUT(temperature);     //DISPLAYS TEMPERATURE OUTPUT
         }
     }
     return 0;
