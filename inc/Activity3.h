3ad9871a43760b1fec58c858eefd5e2e6847b2ef
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 10:43:17 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,1 +1,11 @@
+#ifndef Activity3_H_INCLUDED
+#define Activity3_H_INCLUDED
 
+/**
+ * @brief Function
+ * 
+ */
+void PWMset();
+char PWM_Reading(uint16_t temp_value);
+
+#endif // Activity3_H_INCLUDED
