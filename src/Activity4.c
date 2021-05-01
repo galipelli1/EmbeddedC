fb5afbc9b3dda607731a70418664fe9fe5a61680
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 18:38:52 +0530

    Update Activity4.c

--- src/Activity4.c
+++ src/Activity4.c
@@ -1,10 +1,10 @@
 #include<avr/io.h>
 #include"Activity4.h"
 
-void InitUART(uint16_t br_value){
+void InitUART(uint16_t ubrr_value){
 
-    UBRR0L = br_value; 
-    UBRR0H = (br_value>>8)&(0x00ff);
+    UBRR0L = ubrr_value; 
+    UBRR0H = (ubrr_value>>8)&(0x00ff);
     UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ01);
 
     UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
