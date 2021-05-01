423406050814f53f9bb6e9955f218a8fa5196cc2
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 11:00:06 +0530

    Update Activity4.h

--- inc/Activity4.h
+++ inc/Activity4.h
@@ -1,1 +1,22 @@
+#ifndef Activity4_H_INCLUDED
+#define Activity4_H_INCLUDED
+#include<avr/io.h>
+/**
+ * @brief Inittialize UART serial communication ports
+ * 
+ * @param ubrr 
+ */
+void InitUART(uint16_t br);
+/**
+ * @brief Read characters from UART port
+ * @return char
+ */
+char UARTread();
+/**
+ * @brief Write characters to UART port
+ * 
+ * @param data 
+ */
+void output(char data);
 
+#endif // Activity4_H_INCLUDED
