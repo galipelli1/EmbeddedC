a7caf890aa107a42d64f66f9ebe82300a3dce0ec
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 11:03:45 +0530

    Update Activity3.h

--- inc/Activity3.h
+++ inc/Activity3.h
@@ -1,3 +1,13 @@
+/**
+ * @file Activity3.h
+ * @author galipelli1 (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef Activity3_H_INCLUDED
 #define Activity3_H_INCLUDED
 
