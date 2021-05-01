e71bc512ed1ad9dd0870f36cf0db48edba92f7fb
Galipelli Harika <81825379+galipelli1@users.noreply.github.com>
2021-05-01 10:27:08 +0530

    Update Activity1.h

--- inc/Activity1.h
+++ inc/Activity1.h
@@ -1,4 +1,13 @@
-
+/**
+ * @file Activity1.h
+ * @author galipelli1 (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #ifndef INC_H_INCLUDED
 #define INC_H_INCLUDED
